f4a8a1278de96aa41943d78101d0f7d6a32bab01
Hernan Beiza <hernan@hiperactivo.cl>
2020-12-18 22:16:31 -0300

    Prueba gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -10,28 +10,37 @@ def call(){
 	String paramStage = params.paramStage;
     echo "paramStage ${paramStage}";
 
-	stage('build & test') {
-		//Usar el gradlewrapper, incluido en el repo
-		sh './gradlew clean build'
-	}
-	stage('sonar') {
-		//Nombre en SonarQubeScanner en AdminJenkins/ConfigureTools/SonarQubeScanner
-		def scannerHome = tool 'sonar-scanner';
-		//Nombre en AdminJenkins/Configuración Global/SonarQube Servers
-	    withSonarQubeEnv('sonar') { 
-	    	// If you have configured more than one global server connection, you can specify its name
-			sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+	if (paramStage=="") {
+		echo "Ejecutar todo";
+
+		stage('build & test') {
+			//Usar el gradlewrapper, incluido en el repo
+			sh './gradlew clean build'
 		}
-	}
-	stage('run') {
-		sh 'nohup bash ./gradlew bootRun &'
-	}
-	stage('rest') {
-		//sh './gradle build'
-		sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-	}
-	stage('nexus') {
-		nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+		stage('sonar') {
+			//Nombre en SonarQubeScanner en AdminJenkins/ConfigureTools/SonarQubeScanner
+			def scannerHome = tool 'sonar-scanner';
+			//Nombre en AdminJenkins/Configuración Global/SonarQube Servers
+		    withSonarQubeEnv('sonar') { 
+		    	// If you have configured more than one global server connection, you can specify its name
+				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+			}
+		}
+		stage('run') {
+			sh 'nohup bash ./gradlew bootRun &'
+		}
+		stage('rest') {
+			//sh './gradle build'
+			sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+		}
+		stage('nexus') {
+			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+		}
+		
+	} else {
+
+		echo "Ejecutar solo las configuradas";
+
 	}
 }
 
