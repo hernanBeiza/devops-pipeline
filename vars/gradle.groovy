5115d5d32bed33396f8dfaf056f4847b7eefd3b0
Hernan Beiza <hernan@hiperactivo.cl>
2020-12-18 23:07:40 -0300

    Implementación tarea no encontrada en  gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -21,13 +21,17 @@ def call(){
 }
 
 def etapas(pasadas=['build','test','sonar','run','rest','nexus']){
+    Boolean noEncontrada = false;
+
 	if(pasadas.contains("build") || pasadas.contains("test")){
 		stage('build & test') {
 			echo env.STAGE_NAME
 			//Usar el gradlewrapper, incluido en el repo
 			sh './gradlew clean build'
 		}
-	}
+    } else {
+        noEncontrada = true;
+    }
 	if(pasadas.contains("sonar")){
 		stage('sonar') {
 			echo env.STAGE_NAME
@@ -39,14 +43,18 @@ def etapas(pasadas=['build','test','sonar','run','rest','nexus']){
 				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
 			}
 		}
-	}
+    } else {
+        noEncontrada = true;
+    }
 	if(pasadas.contains("run")){
 		stage('run') {
 			echo env.STAGE_NAME
 
 			sh 'nohup bash ./gradlew bootRun &'
 		}
-	}
+    } else {
+        noEncontrada = true;
+    }
 	if(pasadas.contains("rest")){
 		stage('rest') {
 			echo env.STAGE_NAME
@@ -54,15 +62,21 @@ def etapas(pasadas=['build','test','sonar','run','rest','nexus']){
 			//sh './gradle build'
 			sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
 		}
-	}
+    } else {
+        noEncontrada = true;
+    }
 	if(pasadas.contains("nexus")){
 		stage('nexus') {
 			echo env.STAGE_NAME
 
 			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 		}
-	}
-
+	 } else {
+        noEncontrada = true;
+    }
+    if(noEncontrada){
+        echo "Tarea(s) ${pasadas} no encontrada(s)";
+    }
 }
 
 return this;
