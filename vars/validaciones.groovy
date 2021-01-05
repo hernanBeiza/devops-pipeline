d9c27d5496c9e75ff1f1eb3dbcbccef914eeb15e
Hernan Beiza <hernan@hiperactivo.cl>
2021-01-04 22:36:10 -0300

    Inicio paso a ci-cd

--- vars/validaciones.groovy
+++ vars/validaciones.groovy
@@ -3,9 +3,9 @@ def obtenerTipoDeRama(){
 	//sh "env";
 	def rama = env.GIT_BRANCH;
 	echo "obtenerTipoDeRama ${rama}";
-	if(rama.contains("develop")){
+	if(rama.toLowerCase().contains("release")){
 		return "CD";
-	} else if (rama.contains("feature")){
+	} else if (rama.toLowerCase().contains("feature") || rama.contains("develop")){
 		return "CI";
 	} else {
 		return "Rama no soportada";
