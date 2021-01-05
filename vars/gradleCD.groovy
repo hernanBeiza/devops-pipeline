24cc01862e357c063faff50befa7360b9e760b84
Hernan Beiza <hernan@hiperactivo.cl>
2021-01-05 18:03:35 -0300

    Implementación CD

--- vars/gradleCD.groovy
+++ vars/gradleCD.groovy
@@ -1,5 +1,56 @@
 def iniciar(){
     figlet "GRADLE CD";
+	String paramStage = params.paramStage;
+    echo "paramStage ${paramStage}";
+	if (paramStage=="") {
+		echo "Ejecutar todo";
+		etapas();		
+	} else {
+		echo "Ejecutar solo las configuradas";
+		def pasadas = paramStage.split(":");
+		etapas(pasadas);
+	}
+}
+
+def etapas(pasadas=['downloadNexus','runDownloadedJar','rest','nexusCD']){
+    Boolean noEncontrada = false;
+
+	if(pasadas.contains("downloadNexus")){
+		stage('downloadNexus') {
+			echo env.STAGE_NAME;
+			sh "curl -X GET 'com/devopsusach2020/DevOpsUsach2020/1.0.0-RELEASE/DevOpsUsach2020-1.0.0-RELEASE.jar'";
+		}
+    } else {
+        noEncontrada = true;
+    }
+	if(pasadas.contains("runDownloadedJar")){
+		stage('runDownloadedJar') {			
+			echo env.STAGE_NAME;
+			sh 'nohup bash ./gradlew bootRun &'
+		}
+    } else {
+        noEncontrada = true;
+    }
+	if(pasadas.contains("rest")){
+		stage('rest') {
+			echo env.STAGE_NAME;
+			//sh './gradle build'
+			sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
+		}
+    } else {
+        noEncontrada = true;
+    }
+	if(pasadas.contains("nexusCD")){
+		stage('nexusCD') {
+			echo env.STAGE_NAME;
+			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0-RELEASE']]]
+		}
+	 } else {
+        noEncontrada = true;
+    }
+    if(noEncontrada){
+        echo "Tarea(s) ${pasadas} no encontrada(s)";
+    }
 }
 
 return this;
