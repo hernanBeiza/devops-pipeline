8c353f31ed7c2b32c86d39408152efed6a09f354
Hernan Beiza <hernan@hiperactivo.cl>
2021-01-09 19:20:30 -0300

    Refactor Maven CI y CD

--- vars/mavenCI.groovy
+++ vars/mavenCI.groovy
@@ -69,8 +69,8 @@ def rest(){
     }
 }
 
-def nexus(){
-    stage('nexus') {
+def nexusCI(){
+    stage('nexusCI') {
         echo env.STAGE_NAME
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: './build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
     }
