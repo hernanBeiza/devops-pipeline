38c736c237c78b55f6fbc371a6528a16535a7b32
Hernan Beiza <hernan@hiperactivo.cl>
2021-01-09 19:22:11 -0300

    Refactor Maven CI y CD

--- vars/gradleCD.groovy
+++ vars/gradleCD.groovy
@@ -2,58 +2,27 @@ def iniciar(){
     figlet "GRADLE CD";
 	String paramStage = params.paramStage;
     echo "paramStage ${paramStage}";
-	if (paramStage=="") {
-		echo "Ejecutar todo";
-		etapas();		
-	} else {
-		echo "Ejecutar solo las configuradas";
-		def pasadas = paramStage.split(":");
-		etapas(pasadas);
+    def etapasPasadas = null;
+    if(!paramStage.isEmpty()){
+	    etapasPasadas = paramStage.split(":");
 	}
-}
-
-/*
-def etapas(pasadas=['downloadNexus','runDownloadedJar','rest','nexusCD']){
-    Boolean noEncontrada = false;
-	if(pasadas.contains("downloadNexus")){
-		stage('downloadNexus') {
-			echo env.STAGE_NAME;
-			sh "curl -X GET -u admin:admin http://localhost:8081/repository/test-nexus/com/devopsusach2020/DevOpsUsach2020/${env.VERSION}-DEVELOP/DevOpsUsach2020-${env.VERSION}-DEVELOP.jar -O";
-			//sh "ls -l"
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("runDownloadedJar")){
-		stage('runDownloadedJar') {			
-			echo env.STAGE_NAME;
-			sh 'nohup bash ./gradlew bootRun &'
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("rest")){
-		stage('rest') {
-			echo env.STAGE_NAME;
-			//sh './gradle build'
-			sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("nexusCD")){
-		stage('nexusCD') {
-			echo env.STAGE_NAME;
-			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "./DevOpsUsach2020-${env.VERSION}-DEVELOP.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: "${env.VERSION}-RELEASE"]]]
-		}
-	 } else {
-        noEncontrada = true;
-    }
-    if(noEncontrada){
-        echo "Tarea(s) ${pasadas} no encontrada(s)";
+	//runEjecutar ya que run choca con el nombre definido antes
+	def etapasOriginales = ['downloadNexus','runDownloadedJar','rest','nexusCD'];
+	def etapasValidadas = utils.validarEtapas(etapasPasadas,etapasOriginales);
+	
+    etapasValidadas.each{
+        stage(it){
+            try{
+                //"${it.toLowerCase()}"()
+                "${it}"()
+            }catch(Exception e) {
+            	echo e;
+                //env.MensajeErrorSlack = "Stage ${it.toLowerCase()} tiene problemas : ${e}"
+                //error env.MensajeErrorSlack
+            }
+        }
     }
 }
-*/
 
 def downloadNexus(){
 	stage('downloadNexus') {
--- vars/gradleCI.groovy
+++ vars/gradleCI.groovy
@@ -24,70 +24,7 @@ def iniciar(){
             }
         }
     }
-
 }
-
-//TODO
-//Dividir en funciones
-//Agregar lógica de creación de ramas vía código
-/*
-def ejecutarEtapas(pasadas=['build','test','sonar','run','rest','nexusCI']){
-    Boolean noEncontrada = false;
-
-	if(pasadas.contains("build") || pasadas.contains("test")){
-		stage('build & test') {
-			echo env.STAGE_NAME
-			//Usar el gradlewrapper, incluido en el repo
-			sh './gradlew clean build';
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("sonar")){
-		stage('sonar') {
-			echo env.STAGE_NAME
-			//Nombre en SonarQubeScanner en AdminJenkins/ConfigureTools/SonarQubeScanner
-			def scannerHome = tool 'sonar-scanner';
-			//Nombre en AdminJenkins/Configuración Global/SonarQube Servers
-		    withSonarQubeEnv('sonar') { 
-		    	// If you have configured more than one global server connection, you can specify its name
-				sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-			}
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("run")){
-		stage('run') {
-			echo env.STAGE_NAME
-			sh 'nohup bash ./gradlew bootRun &'
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("rest")){
-		stage('rest') {
-			echo env.STAGE_NAME
-			//sh './gradle build'
-			sh "sleep 30 && curl -X GET 'http://localhost:8082/rest/mscovid/test?msg=testing'"
-		}
-    } else {
-        noEncontrada = true;
-    }
-	if(pasadas.contains("nexusCI")){
-		stage('nexusCI') {
-			echo env.STAGE_NAME
-			//env.GIT_BRANCH;
-			nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: "./build/libs/DevOpsUsach2020-${env.VERSION}.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: "${env.VERSION}-${env.BRANCH_NAME}"]]]
-		}
-	 } else {
-        noEncontrada = true;
-    }
-    if(noEncontrada){
-        echo "Tarea(s) ${pasadas} no encontrada(s)";
-    }
-}
-*/
 
 def build(){
 	stage('build & test') {
